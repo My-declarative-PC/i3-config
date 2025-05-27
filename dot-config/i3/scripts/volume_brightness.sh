f8caa6d65d0bd17b1b3527867ec747b512953914
Sitnikov Timofey <tima.sitnikov@mail.ru>
2025-05-27 21:48:32 +0300

    oбновил скрипт

--- dot-config/i3/scripts/volume_brightness.sh
+++ dot-config/i3/scripts/volume_brightness.sh
@@ -4,10 +4,13 @@
 # taken from here: https://gitlab.com/Nmoleo/i3-volume-brightness-indicator
 
 # See README.md for usage instructions
-bar_color="#7f7fff"
+bar_color="#89b4fa"
 volume_step=1
-brightness_step=2.5
+brightness_step=5
 max_volume=100
+download_album_art=true
+show_album_art=true
+show_music_in_volume_indicator=true
 
 # Uses regex to get volume from pactl
 function get_volume {
@@ -42,28 +45,96 @@ function get_brightness_icon {
     brightness_icon=""
 }
 
-# Displays a volume notification using dunstify
+function get_album_art {
+    url=$(playerctl -f "{{mpris:artUrl}}" metadata)
+    if [[ $url == "file://"* ]]; then
+        album_art="${url/file:\/\//}"
+    elif [[ $url == "http://"* ]] && [[ $download_album_art == "true" ]]; then
+        # Identify filename from URL
+        filename="$(echo $url | sed "s/.*\///")"
+
+        # Download file to /tmp if it doesn't exist
+        if [ ! -f "/tmp/$filename" ]; then
+            wget -O "/tmp/$filename" "$url"
+        fi
+
+        album_art="/tmp/$filename"
+    elif [[ $url == "https://"* ]] && [[ $download_album_art == "true" ]]; then
+        # Identify filename from URL
+        filename="$(echo $url | sed "s/.*\///")"
+
+        # Download file to /tmp if it doesn't exist
+        if [ ! -f "/tmp/$filename" ]; then
+            wget -O "/tmp/$filename" "$url"
+        fi
+
+        album_art="/tmp/$filename"
+    else
+        album_art=""
+    fi
+}
+
+# Displays a volume notification
 function show_volume_notif {
     volume=$(get_mute)
     get_volume_icon
+
+    if [[ $show_music_in_volume_indicator == "true" ]]; then
+        current_song=$(playerctl -f "{{title}} - {{artist}}" metadata)
+
+        if [[ $show_album_art == "true" ]]; then
+            get_album_art
+        fi
+
+        dunstify \
+            --appname "volume control" \
+            --hints string:x-dunst-stack-tag:volume_notif \
+            --hints int:value:$volume \
+            --hints string:hlcolor:$bar_color \
+            --icon "$album_art" \
+            --urgency low \
+            "$volume_icon $volume%" "$current_song"
+    else
+        dunstify \
+            --appname "volume control" \
+            --hints string:x-dunst-stack-tag:volume_notif \
+            --hints int:value:$volume \
+            --hints string:hlcolor:$bar_color \
+            --urgency low \
+            "$volume_icon $volume%"
+    fi
+}
+
+# Displays a music notification
+function show_music_notif {
+    song_title=$(playerctl -f "{{title}}" metadata)
+    song_artist=$(playerctl -f "{{artist}}" metadata)
+    song_album=$(playerctl -f "{{album}}" metadata)
+
+    if [[ $show_album_art == "true" ]]; then
+        get_album_art
+    fi
+
     dunstify \
-        --appname "volume control" \
-        --replace 2593 \
-        --urgency low "$volume_icon $volume%" \
-        --hints int:value:$volume \
-        --hints string:hlcolor:$bar_color
+        --appname "music" \
+        --hints string:x-dunst-stack-tag:music_notif \
+        --icon "$album_art" \
+        --urgency low \
+        "$song_title" "$song_artist - $song_album"
 }
 
 # Displays a brightness notification using dunstify
 function show_brightness_notif {
     brightness=$(get_brightness)
+    echo $brightness
     get_brightness_icon
     dunstify \
         --appname "brightness control" \
-        --replace 2593 \
-        --urgency low "$brightness_icon $brightness%" \
+        --hints string:x-dunst-stack-tag:brightness_notif \
         --hints int:value:$brightness \
-        --hints string:hlcolor:$bar_color
+        --hints string:hlcolor:$bar_color \
+        --urgency low \
+        "$brightness_icon $brightness%"
 }
 
 # Main function - Takes user input, "volume_up", "volume_down", "brightness_up", or "brightness_down"
@@ -102,5 +173,23 @@ brightness_down)
     # Decreases brightness and displays the notification
     xbacklight -dec $brightness_step -time 0
     show_brightness_notif
+    ;;
+
+next_track)
+    # Skips to the next song and displays the notification
+    playerctl next
+    sleep 0.5 && show_music_notif
+    ;;
+
+prev_track)
+    # Skips to the previous song and displays the notification
+    playerctl previous
+    sleep 0.5 && show_music_notif
+    ;;
+
+play_pause)
+    playerctl play-pause
+    show_music_notif
+    # Pauses/resumes playback and displays the notification
     ;;
 esac
